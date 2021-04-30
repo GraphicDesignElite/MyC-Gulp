1f20f4539deafdb8c1ec74485bd4dff8dea9b60a
Garret Bever <gbever@graphicdesignelite.com>
2021-04-30 07:39:43 -0400

    Include additional css in processing
    Add new split css  pipelines and update global pipeline to be more efficient

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('scss', function() {
     .pipe(plumber({errorHandler: onError}))
     .pipe(sass())
     .pipe(prefix())
-    .pipe(rename('global-edit.css'))
+    .pipe(rename('global.css'))
     .pipe(gulp.dest('../css'))
     .pipe(reload({stream:true}))
     .pipe(cssmin())
@@ -43,39 +43,9 @@ gulp.task('scss', function() {
     .pipe(gulp.dest('../css'))
 });
 
-gulp.task('css', function() {
-    var onError = function(err) {
-      notify.onError({
-          title:    "Gulp",
-          subtitle: "CSS Compilation Failure!",
-          message:  "Error: <%= error.message %>",
-          sound:    "Beep"
-      })(err);
-      this.emit('end');
-  };
-  return gulp.src('../css/global-edit.css')
-    .pipe(plumber({errorHandler: onError}))
-    .pipe(prefix())
-    .pipe(rename('global.css'))
-    .pipe(gulp.dest('../css'))
-    .pipe(reload({stream:true}))
-    .pipe(cssmin())
-    .pipe(size({ gzip: true, showFiles: true }))
-    .pipe(gulp.dest('../css'))
 
-});
 
-gulp.task('js', function() {
-  return gulp.src('../js/master-edit.js')
-    .pipe(uglify())
-    .pipe(concat('master.js'))
-    .pipe(gulp.dest('../js'))
-});
-gulp.task('jshint', function() {
-    gulp.src('../js/master.js')
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'));
-});
+
 
 gulp.task('scss-lint', function() {
   gulp.src('scss/**/*.scss')
@@ -83,45 +53,272 @@ gulp.task('scss-lint', function() {
     .pipe(scsslint());
 });
 
-//css rename for cache bust
-gulp.task("revision:renameCss", ["css"], function(){
-  return gulp.src(["../css/global.css"])
-  .pipe(rev())
-  .pipe(gulp.dest('../cdn/css'))
-  .pipe(rev.manifest({ path: "manifest.json" }))
-  .pipe(revdel({ dest: '../cdn/css', force: true }))
-  .pipe(gulp.dest('../cdn/css'))
-});
-
-gulp.task("revision:updateReferencesCss", ["css","revision:renameCss"], function(){
-   return gulp.src(["../cdn/css/manifest.json","../templates/common/header_includes.jsp"])
-   .pipe(collect({ replaceReved: true }))
-   .pipe(gulp.dest("../templates/common/"))
-});
-
-//js rename for cache bust
-gulp.task("revision:renameJs", ["js"], function(){
-    return gulp.src(["../js/master.js"])
+//Global
+gulp.task('css-global', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Global CSS Compilation Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+  return gulp.src('../css/global.css')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(prefix())
+   
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+    
     .pipe(rev())
+    .pipe(gulp.dest('../cdn/css'))
+    .pipe(rev.manifest({ path: "manifest.json" }))
+    .pipe(revdel({ dest: '../cdn/css', force: true }))
+    .pipe(gulp.dest('../cdn/css'))
+});
+
+gulp.task("revision:updateReferencesCssGlobal", ["css-global"], function(){
+     return gulp.src(["../cdn/css/manifest.json","../templates/common/header_includes.jsp"])
+     .pipe(collect({ replaceReved: true }))
+     .pipe(gulp.dest("../templates/common/"))
+});
+
+
+
+
+gulp.task('css-home', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Home Page CSS Compilation Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+  return gulp.src('../css/templates/home-page.css')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(prefix())
+ 
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+
+    .pipe(rev())
+    .pipe(gulp.dest('../cdn/css/templates'))
+    .pipe(rev.manifest({ path: "manifest-home-page.json" }))
+    .pipe(revdel({ dest: '../cdn/css/templates', force: true }))
+    .pipe(gulp.dest('../cdn/css/templates'))
+});
+gulp.task("revision:updateReferencesCssHome", ["css-home"], function(){
+    return gulp.src(["../cdn/css/templates/manifest-home-page.json","../templates/categories/home_page_template.jsp"])
+    .pipe(collect({ replaceReved: true }))
+    .pipe(gulp.dest("../templates/categories"))
+});
+
+
+//common Category
+gulp.task('css-common-category', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Common Category CSS Compilation Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+  return gulp.src('../css/templates/common-category.css')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(prefix())
+ 
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+
+    .pipe(rev())
+    .pipe(gulp.dest('../cdn/css/templates'))
+    .pipe(rev.manifest({ path: "manifest-common-category.json" }))
+    .pipe(revdel({ dest: '../cdn/css/templates', force: true }))
+    .pipe(gulp.dest('../cdn/css/templates'))
+});
+gulp.task("revision:updateReferencesCssCommonCategory", ["css-common-category"], function(){
+    return gulp.src(["../cdn/css/templates/manifest-common-category.json","../templates/categories/category_common_template.jsp"])
+    .pipe(collect({ replaceReved: true }))
+    .pipe(gulp.dest("../templates/categories"))
+});
+
+//Product Page
+gulp.task('css-product-page', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Product Page CSS Compilation Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+  return gulp.src('../css/templates/product-page.css')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(prefix())
+ 
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+
+    .pipe(rev())
+    .pipe(gulp.dest('../cdn/css/templates'))
+    .pipe(rev.manifest({ path: "manifest-product-page.json" }))
+    .pipe(revdel({ dest: '../cdn/css/templates', force: true }))
+    .pipe(gulp.dest('../cdn/css/templates'))
+});
+gulp.task("revision:updateReferencesCssProductPage", ["css-product-page"], function(){
+    return gulp.src(["../cdn/css/templates/manifest-product-page.json","../templates/categories/product-page-template.jsp"])
+    .pipe(collect({ replaceReved: true }))
+    .pipe(gulp.dest("../templates/categories"))
+});
+
+
+
+//Learning Center
+gulp.task('css-learning-center', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Learning Center CSS Compilation Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+  return gulp.src('../css/templates/learning-center.css')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(prefix())
+ 
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+
+    .pipe(rev())
+    .pipe(gulp.dest('../cdn/css/templates'))
+    .pipe(rev.manifest({ path: "manifest-learning-center.json" }))
+    .pipe(revdel({ dest: '../cdn/css/templates', force: true }))
+    .pipe(gulp.dest('../cdn/css/templates'))
+});
+gulp.task("revision:updateReferencesCssLearningCenter", ["css-learning-center"], function(){
+    return gulp.src(["../cdn/css/templates/manifest-learning-center.json","../templates/categories/learning_center_template.jsp"])
+    .pipe(collect({ replaceReved: true }))
+    .pipe(gulp.dest("../templates/categories"))
+});
+
+//State Hubs
+gulp.task('css-state-hub', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Learning Center CSS Compilation Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+  return gulp.src('../css/templates/state-hub.css')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(prefix())
+ 
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+
+    .pipe(rev())
+    .pipe(gulp.dest('../cdn/css/templates'))
+    .pipe(rev.manifest({ path: "manifest-state-hub.json" }))
+    .pipe(revdel({ dest: '../cdn/css/templates', force: true }))
+    .pipe(gulp.dest('../cdn/css/templates'))
+});
+gulp.task("revision:updateReferencesCssStateHub", ["css-state-hub"], function(){
+    return gulp.src(["../cdn/css/templates/manifest-state-hub.json","../templates/categories/state_hub_template.jsp", "../templates/categories/state_info_template.jsp"])
+    .pipe(collect({ replaceReved: true }))
+    .pipe(gulp.dest("../templates/categories"))
+});
+
+//About Us
+gulp.task('css-about-us', function() {
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Learning Center CSS Compilation Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
+  };
+  return gulp.src('../css/templates/about-us.css')
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(prefix())
+ 
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+
+    .pipe(rev())
+    .pipe(gulp.dest('../cdn/css/templates'))
+    .pipe(rev.manifest({ path: "manifest-about-us.json" }))
+    .pipe(revdel({ dest: '../cdn/css/templates', force: true }))
+    .pipe(gulp.dest('../cdn/css/templates'))
+});
+gulp.task("revision:updateReferencesCssAboutUs", ["css-about-us"], function(){
+    return gulp.src(["../cdn/css/templates/manifest-about-us.json","../templates/categories/about-us-template.jsp"])
+    .pipe(collect({ replaceReved: true }))
+    .pipe(gulp.dest("../templates/categories"))
+});
+
+
+
+gulp.task('js', function() {
+    return gulp.src('../js/master.js')
+      .pipe(uglify())
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      
+      .pipe(concat('master.js'))
+      .pipe(rev())
     .pipe(gulp.dest('../cdn/js'))
     .pipe(rev.manifest({ path: "manifest.json" }))
     .pipe(revdel({ dest: '../cdn/js', force: true }))
     .pipe(gulp.dest('../cdn/js'))
+     
   });
-  
-gulp.task("revision:updateReferencesJs", ["js","revision:renameJs"], function(){
+ 
+//js rename for cache bust  
+gulp.task("revision:updateReferencesJs", ["js"], function(){
      return gulp.src(["../cdn/js/manifest.json","../templates/common/footer_includes.jsp"])
      .pipe(collect({ replaceReved: true }))
      .pipe(gulp.dest("../templates/common/"))
-  });
+});
+
+
+
+
+
+
 
 
 
 
 gulp.task('watch', function() {
-  gulp.watch('../css/global-edit.css', ['css', 'revision:renameCss', 'revision:updateReferencesCss']);
-  gulp.watch('../js/master-edit.js', ['jshint', 'js', 'revision:renameJs', 'revision:updateReferencesJs']);
-
+  
+  gulp.watch('../js/master.js', ['js', 'revision:updateReferencesJs']);
+  gulp.watch('../css/global.css', ['css-global', 'revision:updateReferencesCssGlobal']);
+  gulp.watch('../css/templates/home-page.css', ['css-home', 'revision:updateReferencesCssHome']);
+  gulp.watch('../css/templates/common-category.css', ['css-common-category', 'revision:updateReferencesCssCommonCategory']);
+  gulp.watch('../css/templates/product-page.css', ['css-product-page', 'revision:updateReferencesCssProductPage']);
+  gulp.watch('../css/templates/learning-center.css', ['css-learning-center', 'revision:updateReferencesCssLearningCenter']);
+  gulp.watch('../css/templates/state-hub.css', ['css-state-hub', 'revision:updateReferencesCssStateHub']);
+  gulp.watch('../css/templates/about-us.css', ['css-about-us', 'revision:updateReferencesCssAboutUs']);
 });
 
-gulp.task('default', ['css', 'revision:renameCss', 'revision:updateReferencesCss', 'js', 'revision:renameJs', 'revision:updateReferencesJs', 'watch']);
+gulp.task('default', ['watch']);
